11ecd18e336e6e8da5590cf914ca8640d4bf3f89
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-05-27 20:25:24 +0200

    lab done

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('views'));
+app.use(express.static('public'));
 
 
 app.get('/', (request, response, next) => {response.sendFile(__dirname + "/views/home.html")
  })
 
+ app.get('/about', (request, response, next) => {response.sendFile(__dirname + "/views/about.html")
+ })
+
+ app.get('/work', (request, response, next) => {response.sendFile(__dirname + "/views/works.html")
+})
+
+app.get('/gallery', (request, response, next) => {response.sendFile(__dirname + "/views/gallery.html")
+})
 
 // Server listening
 app.listen(3000, () => console.log('🚀 My first app listening on port 3000! '));
